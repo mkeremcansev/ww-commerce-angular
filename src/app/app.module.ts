c01e62d32bd24eb915de8a55180a6f88cdcf9242
mkeremcansev <mkeremcansev@gmail.com>
2023-02-01 18:57:01 +0300

    main: general component paths updated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
-import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
+import { NotfoundComponent } from './feature/components/notfound/notfound.component';
+import { ProductService } from './feature/service/product.service';
+import { CountryService } from './feature/service/country.service';
+import { CustomerService } from './feature/service/customer.service';
+import { EventService } from './feature/service/event.service';
+import { IconService } from './feature/service/icon.service';
+import { NodeService } from './feature/service/node.service';
+import { PhotoService } from './feature/service/photo.service';
 
 @NgModule({
     declarations: [
