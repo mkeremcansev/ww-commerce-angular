f4aae007c9a590595fb0438b6506f3885ab18d2d
Mustafa Kerem CANSEV <76810832+mkeremcansev@users.noreply.github.com>
2023-04-08 16:58:20 +0300

    main: logout struct created.

--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {LocalStorageService} from "../storage/local-storage.service";
 import {Router} from "@angular/router";
 import {RedirectService} from "../redirect/redirect.service";
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
     providedIn: 'root'
@@ -11,10 +13,12 @@ export class UserService {
     /**
      * @param storageService
      * @param redirectService
+     * @param httpClient
      */
     constructor(
         public storageService: LocalStorageService,
-        public redirectService: RedirectService
+        public redirectService: RedirectService,
+        public httpClient: HttpClient
     ) {
     }
 
@@ -22,6 +26,12 @@ export class UserService {
      * @method logout
      */
     logout() {
-        this.redirectService.redirect('/login/auth', 3);
+        this.httpClient.get(environment.api + '/user/logout').subscribe(response => {
+                this.redirectService.redirect('/login/auth', 3);
+            },
+            error => {
+                this.redirectService.redirect('/login/auth', 3);
+            });
+
     }
 }
